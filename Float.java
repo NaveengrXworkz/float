f2f4d5a00d1365a36e907fe6b9779448c77e01dd
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 21:29:41 +0530

    float programme

---
+++ Float.java
@@ -0,0 +1,15 @@
+class Float{
+     static float[] height = {5.7f,7f,6.11f,5.11f,6.2f,5.4f,5.9f,4.11f,5f,5.6f,};
+     public static void main(String[] args){
+     System.out.println(height[0]);
+     System.out.println(height[1]);
+     System.out.println(height[2]);
+     System.out.println(height[3]);
+     System.out.println(height[4]);
+     System.out.println(height[5]);
+     System.out.println(height[6]);
+     System.out.println(height[7]);
+     System.out.println(height[8]);
+     System.out.println(height[9]);
+     }
+}
